b70ccad5bb4a6ab1d4e2741be97ffb832cb29626
SrivallikaNatteti <srivallikanatteti@gmail.com>
2023-08-20 12:18:28 +0530

    operators
    operatorcodes

---
+++ ByteDataTypeDemo.java
@@ -0,0 +1,12 @@
+class ByteDataTypeDemo
+{
+  public static void main(String[] args)
+  {
+   // byte b=127;
+  // byte b= -128;
+  // byte b= 128;
+ // byte b= -129;
+  byte b= false;
+	System.out.println("the value of b=" + b);
+  }
+}
---
+++ PreincrementOPeratorDemo.java
@@ -0,0 +1,10 @@
+class PreincrementOPeratorDemo
+{
+  public static void main(String[] args)
+   {
+     int a=10,b;
+	 b=++a + ++a;
+	 System.out.println("a=" +a);
+	 System.out.println("b=" +b);
+	 }
+}
